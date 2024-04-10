1e7ecedb05147d25986ba9409543961b47153ff5
helpingstar <iamhelpingstar@gmail.com>
2024-04-10 15:53:34 +0900

    Update experiment

--- woodoku_ppo_v2_action_mask.py
+++ woodoku_ppo_v2_action_mask.py
@@ -40,7 +40,7 @@ class Args:
     # Algorithm specific arguments
     env_id: str = "gym_woodoku/Woodoku-v0"
     """the id of the environment"""
-    total_timesteps: int = 1000000000
+    total_timesteps: int = 3000000000
     """total timesteps of the experiments"""
     learning_rate: float = 2.5e-4
     """the learning rate of the optimizer"""
@@ -48,7 +48,7 @@ class Args:
     """the number of parallel game environments"""
     num_steps: int = 128
     """the number of steps to run in each environment per policy rollout"""
-    anneal_lr: bool = True
+    anneal_lr: bool = False
     """Toggle learning rate annealing for policy and value networks"""
     gamma: float = 0.99
     """the discount factor gamma"""
@@ -216,7 +216,7 @@ if __name__ == "__main__":
 
     agent = Agent(envs).to(device)
     if args.load_model:
-        agent.load_state_dict(torch.load(f"runs/gym_woodoku/{args.load_model}"))
+        agent.load_state_dict(torch.load(f"{args.load_model}"))
     optimizer = optim.Adam(agent.parameters(), lr=args.learning_rate, eps=1e-5)
 
     # ALGO Logic: Storage setup
